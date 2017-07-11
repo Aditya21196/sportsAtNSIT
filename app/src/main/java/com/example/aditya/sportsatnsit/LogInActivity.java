032349033922a0689f5f30bff37fb2cd9e7f4907
Karanjot Singh <karanjot21@gmail.com>
2017-07-12 03:31:30 +0530

    removed log statement

--- app/src/main/java/com/example/aditya/sportsatnsit/LogInActivity.java
+++ app/src/main/java/com/example/aditya/sportsatnsit/LogInActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -34,7 +33,6 @@ public class LogInActivity extends AppCompatActivity {
                 editor.apply();
 
                 MainActivity.YEAR = sharedPreferences.getString("year", "na");
-                Log.i("Login", MainActivity.YEAR);
                 MainActivity.BRANCH = sharedPreferences.getString("branch", "na");
                 MainActivity.SECTION = sharedPreferences.getInt("section", -1);
 
