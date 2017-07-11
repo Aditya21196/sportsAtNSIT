69ff921f071b598db03db2f6e8c80afc8142afbb
Aditya <aditya21196@gmail.com>
2017-07-12 00:42:47 +0530

    first commit

--- app/src/main/java/com/example/aditya/sportsatnsit/MainActivity.java
+++ app/src/main/java/com/example/aditya/sportsatnsit/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.aditya.sportsatnsit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+    private static final int GPS_ERRORDIALOG_REQUEST = 9001 ;
+    private static int TIME_OUT = 3000; //Time to launch the another activity
+
+
+
+//change
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, FirebaseActivity.class);
+                startActivity(i);
+                finish();
+            }
+
+
+        }, TIME_OUT);
+
+
     }
 }
